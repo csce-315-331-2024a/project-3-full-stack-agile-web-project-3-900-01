e0d24be05a8b250e342163a0963cf779cba5fcb7
Easton <havemeas@tamu.edu>
2024-03-31 17:36:53 -0500

    checkout button implementation

--- RevsDjango/orders/views.py
+++ RevsDjango/orders/views.py
@@ -4,23 +4,41 @@ from django.urls import reverse
 from django.http import HttpResponse, JsonResponse
 from django.db import connection
 
-# # Create your views here.
-# def orders(request):
-#     return render(request, 'orders/orders.html')
+# Global variables for tracking order information
+currentPrice = 0.0
+currentOrder = []
+orderID = 1
+
+
+# Class to store all information on a menu item
+class MenuItem:
+    def __init__(self, description, price, id, category):
+        self.description = description
+        self.price = price
+        self.id = id
+        self.category = category
+
 
 # Initializes all the menu items buttons
 def orders(request):
+    global orderID
+
     with connection.cursor() as cursor:
+        #gets next order id
+        cursor.execute("SELECT MAX(id) FROM orders")
+        orderID = cursor.fetchone()[0] + 1
+
         # Gets a list of all the menu items and sorts in alphabetical order
-        cursor.execute("SELECT description FROM menu_items")
+        cursor.execute("SELECT description, price, id, category FROM menu_items")
         data = cursor.fetchall()
         data.sort()
-        buttonData = [{'description': currentItem[0]} for currentItem in data]
+        buttonData = [{'description': currentItem[0], 'price': currentItem[1], 'id': currentItem[2],
+                        'category': currentItem[3]} for currentItem in data]
 
         # Categorize buttons based on their descriptions
-        categorized_buttons = {
+        categorizedButtons = {
             'Burgers': [],
-            'Baskets': [],
+            'Value Meals': [],
             'Sandwiches': [],
             'Shakes': [],
             'Beverages': [],
@@ -28,24 +46,67 @@ def orders(request):
         }
 
         for button in buttonData:
-            if 'Burger' in button['description']:
-                categorized_buttons['Burgers'].append(button)
-            elif 'Tender' in button['description'] or 'Meal' in button['description']:
-                categorized_buttons['Baskets'].append(button)
-            elif 'Sandwich' in button['description'] or 'Wrap' in button['description'] or 'Patty' in button['description']:
-                categorized_buttons['Sandwiches'].append(button)
-            elif 'Shake' in button['description'] or 'Ice' in button['description']:
-                categorized_buttons['Shakes'].append(button)
-            elif 'Drink' in button['description'] or'Water' in button['description'] or 'Beer' in button['description']:
-                categorized_buttons['Beverages'].append(button)
+            if button['category'] == 'Burger':
+                categorizedButtons['Burgers'].append(button)
+            elif button['category'] == 'Value Meal':
+                categorizedButtons['Value Meals'].append(button)
+            elif button['category'] == 'Sandwiches':
+                categorizedButtons['Sandwiches'].append(button)
+            elif button['category'] == 'Shakes/More':
+                categorizedButtons['Shakes'].append(button)
+            elif button['category'] == 'Drink':
+                categorizedButtons['Beverages'].append(button)
             else:
-                categorized_buttons['Sides'].append(button)
+                categorizedButtons['Sides'].append(button)
 
-        context = {'categorized_buttons': categorized_buttons}
+        context = {'categorizedButtons': categorizedButtons}
 
-        # context = {'buttonData': buttonData}
         return render(request, 'orders/orders.html', context)
 
 
+# Adds menu item button functionality
+def addItem(request):
+    global currentPrice
+    global currentOrder
+
+    # Extracts all the menu item's information
+    data = json.loads(request.body)
+    description = data.get('description')
+    price = data.get('price')
+    id = data.get('id')
+    category = data.get('category')
+
+    # Updates all the global variables for the order
+    currentPrice += float(price)
+    currentMenuItem = MenuItem(description, price, id, category)
+    currentOrder.append(currentMenuItem)
+
+    return JsonResponse({'message': 'Item Added'})
+
+#adds order placement functionality
+def payNow(request):
+    customerID = 1
+    employeeID = 1111
+    orderTime = "1/1/2024 9:00"
+
+    #list of all food items in order
+    foodItems = []
+    for menuItem in currentOrder:
+        foodItems += menuItem.id
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO orders (order_id, customer_id, employee_id, total_price, order_time) VALUES (%s, %s, %s, %s, %s)",
+            [orderID, customerID, employeeID, currentPrice, orderTime]
+        )
+        for foodItem in foodItems:
+            cursor.execute(
+                "INSERT INTO order_breakout (order_id, food_items) VALUES (%s, %s)",
+                [orderID, foodItem]
+            )
+    return JsonResponse({'message': 'Checked Out'})
+
 def cart_view(request):
+    # Your logic to retrieve the shopping cart items and process the request goes here
+    # For now, let's just render the empty cart.html template
     return render(request, 'orders/cart.html')
